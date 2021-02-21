913d000abbdd81a24946d4b42657bd0fad58dbb9
laugh0608 <laugh0608@foxmail.com>
2021-02-21 13:20:58 +0800

    2021-02-21 13:20:56

--- live2d/index.js
+++ live2d/index.js
@@ -9,12 +9,12 @@ function browserRedirect() {
     var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
     if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {} else {
-        document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" />');
-        document.write('<div id="landlord"><div class="message" style="opacity:0"></div><canvas id="live2d" width="180" height="150" class="live2d"></canvas><div class="hide-button">隐藏</div></div>');
-        document.write('<script type="text/javascript">var message_Path = "/live2d/";var home_Path = "https://imbhj.com/"</script>');
+        document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" /><div id="landlord"> <div class="message" style="opacity:0.5"></div> <canvas id="live2d" width="180" height="150" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
+        document.write('<script type="text/javascript"> var message_Path = "/live2d/"</script>');
+        document.write('<script type="text/javascript"> var home_Path = "https://imbhj.com/" </script>');
         document.write('<script type="text/javascript" src="/live2d/js/live2d.js"></script>');
         document.write('<script type="text/javascript" src="/live2d/js/message.js"></script>');
-        document.write('<script type="text/javascript">loadlive2d("live2d", "/live2d/model/tia/model.json");</script>');
+        document.write('<script type="text/javascript"> loadlive2d("live2d", "/live2d/model/tia/model.json");</script>');
     }
 }
 browserRedirect();
