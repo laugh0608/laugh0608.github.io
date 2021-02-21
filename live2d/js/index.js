b866463247f4234ad7e57d942fa71c04ccc33659
laugh0608 <laugh0608@foxmail.com>
2021-02-21 11:01:02 +0800

    2021-02-21 11:01:00

--- live2d/js/index.js
+++ live2d/js/index.js
@@ -10,7 +10,7 @@ function browserRedirect() {
     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
     if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {} else {
         document.write('<link rel="stylesheet" href="/live2d/css/live2d.css" /><div id="landlord"> <div class="message" style="opacity:0.5"></div> <canvas id="live2d" width="180" height="150" class="live2d"></canvas> <div class="hide-button"><font color="#FFFFFF">隐藏</font></div></div>');
-        document.write('<script type="text/javascript"> var message_Path = "/live2d/"</script>');
+        document.write('<script type="text/javascript"> var message_Path = "/live2d/message.json"</script>');
         document.write('<script type="text/javascript"> var home_Path = "https://imbhj.com/" </script>');
         document.write('<script type="text/javascript" src="/live2d/js/live2d.js"></script>');
         document.write('<script type="text/javascript" src="/live2d/js/message.js"></script>');
